ac0d6fe6f3969c1a61d786de0773be9d10f0fae6
Keshden <bleonrey7@gmail.com>
2021-05-08 14:25:15 -0600

    deleted last migrate as well to solidify the master branch going forward

--- db/migrate/20210507082408_add_teach_and_student_to_courses.rb
+++
@@ -1,6 +0,0 @@
-class AddTeachAndStudentToCourses < ActiveRecord::Migration[6.1]
-  def change
-    add_column :courses, :teacher_id, :integer
-    add_column :courses, :student_id, :integer
-  end
-end
